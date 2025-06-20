3f8bc110468aa9cebae20bdf526d564c66eb3419
Justin Glazer <jglazer365@gmail.com>
2025-06-19 21:46:15 -0400

    added constructors for tile class

--- TowerDefense/src/Tile.java
+++ TowerDefense/src/Tile.java
@@ -1,4 +1,36 @@
 
 public class Tile {
+	
+	//instance variables
+	private boolean containsPiece;
+	private int width;
+	private int x, y;
+	private int bg;
+	
+	//CONSTRUCTORS
+	public Tile () {
+		this.containsPiece = false;
+	}
+	
+	public Tile(int x, int y, int width) {
+		this.width = width;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Tile(int x, int y, int width, boolean containsPiece) {
+		this.width = width;
+		this.x = x;
+		this.y = y;
+		this.containsPiece = containsPiece;
+	}
+	
+	public Tile(int x, int y, int width, boolean containsPiece, int bg) {
+		this.width = width;
+		this.x = x;
+		this.y = y;
+		this.containsPiece = containsPiece;
+		this.bg = bg;
+	}
 
 }
